f70f974f8d22627a4be60460d51dae29fe84342c
Gio300 <giovanni.monco00@gmail.com>
2024-07-11 14:04:12 +0200

    feat: Fix import error in Evaluation_loop.py for Kaggle

--- models/Evaluation_loop.py
+++ models/Evaluation_loop.py
@@ -2,9 +2,11 @@
 import faiss
 import faiss.contrib.torch_utils
 import numpy as np
+import sys
 import torch
 from prettytable import PrettyTable
-from visualization.Visualization import print_preds
+sys.path.append('/kaggle/input/modules/visualization')
+from Visualization import print_preds
 
 # Setup device agnostic code
 device = "cuda" if torch.cuda.is_available() else "cpu"
